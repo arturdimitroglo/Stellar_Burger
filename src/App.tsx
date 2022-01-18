962eacaae1efa9ee2e1eee80060d14af8d98f0e0
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-01-18 13:32:12 +0300

    commit v-0.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
-export default App;
+  const URL = "https://norma.nomoreparties.space/api/ingredients";
+  const [ingredients, setIngredients] = React.useState([]);
+
+  React.useEffect(
+    () => {
+      fetch(`${URL}`, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => {
+        return res.json()
+      }).then(res => {
+        setIngredients(res.data)
+      }).catch(err =>
+        console.log(err)
+      )
+    }, [])
+
+  return (
+    <div className='App'>
+      <AppHeader />
+      <div className={style.border}>
+        <div className='mt-10'>
+          <p className='text text_type_main-large'>Соберите бургер</p>
+
+          <BurgerIngredients ingredients={ingredients} />
+        </div>
+
+        <>
+          <BurgerConstructor data={ingredients} />
+        </>
+      </div>
+    </div>
+  )
+}
