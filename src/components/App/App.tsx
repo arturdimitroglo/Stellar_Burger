543f9ed967ddcbc69a3bdd4a2a8582d65e530507
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-04-24 06:55:58 +0300

    error test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -45,7 +45,7 @@ const App: FC = () => {
   return (
     <>
       <Routes location={state?.background || location}>
-        <Route path='/react-burger/*' element={<Layout />}>
+        <Route path='*' element={<Layout />}>
           <Route index element={<Main />} />
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Registration />} />
